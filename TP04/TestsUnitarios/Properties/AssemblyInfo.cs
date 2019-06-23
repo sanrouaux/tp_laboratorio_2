76e945eb495fd6281626d3c57f7f63f03c845b24
santiago rouaux <sanrouaux@hotmail.com>
2019-06-23 20:46:39 -0300

    Incluyo TP04

---
+++ TP04/TestsUnitarios/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestsUnitarios")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestsUnitarios")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ab769cdd-a4cf-41b7-859a-a7c3785cadb3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TP04/TestsUnitarios/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+
+namespace TestsUnitarios
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// Comprueba que la lista de paquetes de un correo esté instanciada
+        /// </summary>
+        [TestMethod]
+        public void ListaDePaquetesNula()
+        {
+            Correo c = new Correo();
+            Assert.IsNotNull(c.Paquetes);      
+        }
+
+        /// <summary>
+        /// Comprueba que no se pueden cargar dos paquetes con el mismo trackingID y, al intentar hacerlo, que se lance una
+        /// excepción del tipo TrackingIdRepetidoException 
+        /// </summary>
+        [TestMethod]
+        public void DosPaquetesConMismoTrackingID()
+        {
+            Correo c = new Correo();
+            Paquete paq1 = new Paquete("Belgrano 1224", "123-543-2345");
+            Paquete paq2 = new Paquete("San Martin 987", "123-543-2345");
+            
+            try
+            {
+                c += paq1;
+                c += paq2;
+                Assert.Fail("Error. Carga dos paquetes con el mismo TrackingID");
+            }
+            catch(Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(TrackingIdRepetidoException));
+            }
+        }
+    }
+}
